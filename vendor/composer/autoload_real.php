577614566d32f81a4c728c4ccfca0c76b229216f
Shazzad Hossain Khan <sajib1223@gmail.com>
2023-02-28 20:54:53 +0600

    exclude unused dependencies. add stripe sdk. exclude vendor folder from github

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInit02b12d70543487b42c7541b905803899
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit02b12d70543487b42c7541b905803899::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
