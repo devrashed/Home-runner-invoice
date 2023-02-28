02c70ebe3043a48afd65cb43a1dd4c42ad759f63
Shazzad Hossain Khan <sajib1223@gmail.com>
2023-02-28 20:56:21 +0600

    xr

--- inv_bill_home.php
+++ inv_bill_home.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Home Runner Invoicing
- * Description: Home Runner Invoicing
+ * Plugin Name: Homerunner Invoicing
+ * Description: Homerunner Invoicing
  * Plugin URI: https://hudsoncreative.com/
  * Author: Rashed khan
  * Version: 1.0
@@ -9,15 +9,9 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html 
  */
 
-if ( ! defined('ABSPATH')) exit;  // if direct access 
-
-
-require_once 'inv_bill_setting_page.php'; 
-
-require_once 'inv_bill_user_registration_form.php';
-require_once 'inv_bill_user_login.php';
-
-
+if (!defined('ABSPATH')) {
+   exit;
+}
 
 final class invintegrate
 {
@@ -26,82 +20,83 @@ final class invintegrate
 
    function __construct()
    {
+      $this->invintegrate_define_constants(); //define asstes
 
-    $this->invintegrate_define_constants(); //define asstes
+      register_activation_hook(invINTEGARTE_ASSETS_FILE, [$this, 'invintegrate_assets_activate']); // plugin activation
 
-    register_activation_hook( invINTEGARTE_ASSETS_FILE, [ $this, 'invintegrate_assets_activate' ] ); // plugin activation
-    
-   add_action( 'plugins_loaded', [$this, 'invintegrate_assets_init_plugin' ] );  // load plugin
-  
-   add_action( 'wp_enqueue_scripts', [$this, 'inv_register_assets']);  
+      add_action('plugins_loaded', [$this, 'invintegrate_assets_init_plugin']); // load plugin
 
-   //add_action('admin_enqueue_scripts', [$this, 'admin_inv_register_assets']);
- }
- 
- function inv_register_assets(){
-   wp_enqueue_script('jquery');
-  /* wp_enqueue_script( 'ChosenJs' , plugins_url('/assets/js/chosen.jquery.min.js' , __FILE__ ) , array('jquery'), '1.0.0', false);*/
-   wp_enqueue_script( 'jQuery-min', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', 0.122, true );
+      add_action('wp_enqueue_scripts', [$this, 'inv_register_assets']);
 
-   wp_enqueue_script( 'inv_custom', plugins_url( 'assets/js/custom.js', __FILE__ ), array( 'jquery' ), 0.3,false );
-
-   wp_localize_script( 'inv_custom', 'invo_ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
-   
-}  
-
-/* function admin_inv_register_assets(){
-
-   wp_enqueue_script( 'ChosenJs' , plugins_url('/assets/js/chosen.jquery.min.js' , __FILE__ ) , array('jquery'), '1.0.0', false);
-
-   wp_enqueue_script( 'admin_custom', plugins_url( 'assets/js/admin_custom.js', __FILE__ ), array( 'jquery' ), 0.3,false );
-
-   wp_localize_script( 'admin_custom', 'admin_ajax_object', array( 'ajax-url' => admin_url( 'admin-ajax.php' ) ) );
-   
-}*/
-     
-
-    /**
-     * Define the required plugin constants
-     *
-     * @return void
-     */ 
-
-    public function invintegrate_define_constants() {
-       define( 'invINTEGRATE_ASSETS_VERSION', self::version );
-       define( 'invINTEGARTE_ASSETS_FILE', __FILE__ );
-       define( 'invINTEGARTE_ASSETS_PATH', __DIR__ );
-       define( 'invINTEGARTE_ASSETS_URL', plugin_dir_url( __FILE__  ) );
-    }
-
-    public function invintegrate_assets_init_plugin() {
-
-        new invSettimngPage(); 
-        new userresgistration();
-        new userlogin();
-        
+      //add_action('admin_enqueue_scripts', [$this, 'admin_inv_register_assets']);
    }
 
-     /**
-     * Plugin Activatoin Hook
-     */  
-     
-     public function invintegrate_assets_activate() {
+   function inv_register_assets()
+   {
+      wp_enqueue_script('jquery');
+      /* wp_enqueue_script( 'ChosenJs' , plugins_url('/assets/js/chosen.jquery.min.js' , __FILE__ ) , array('jquery'), '1.0.0', false);*/
+      wp_enqueue_script('jQuery-min', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', 0.122, true);
 
-       $installed = get_option( 'invintegrate_installed' );
+      wp_enqueue_script('inv_custom', plugins_url('assets/js/custom.js', __FILE__), array('jquery'), 0.3, false);
 
-       if ( ! $installed ) {
-         update_option( 'invintegrate_installed', time() );
+      wp_localize_script('inv_custom', 'invo_ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
+
+   }
+
+   /* function admin_inv_register_assets(){
+   wp_enqueue_script( 'ChosenJs' , plugins_url('/assets/js/chosen.jquery.min.js' , __FILE__ ) , array('jquery'), '1.0.0', false);
+   wp_enqueue_script( 'admin_custom', plugins_url( 'assets/js/admin_custom.js', __FILE__ ), array( 'jquery' ), 0.3,false );
+   wp_localize_script( 'admin_custom', 'admin_ajax_object', array( 'ajax-url' => admin_url( 'admin-ajax.php' ) ) );
+   
+   }*/
+
+
+   /**
+    * Define the required plugin constants
+    *
+    * @return void
+    */
+   public function invintegrate_define_constants()
+   {
+      define('invINTEGRATE_ASSETS_VERSION', self::version);
+      define('invINTEGARTE_ASSETS_FILE', __FILE__);
+      define('invINTEGARTE_ASSETS_PATH', __DIR__);
+      define('invINTEGARTE_ASSETS_URL', plugin_dir_url(__FILE__));
+   }
+
+   public function invintegrate_assets_init_plugin()
+   {
+      require_once 'vendor/autoload.php';
+      require_once 'inv_bill_setting_page.php';
+      require_once 'inv_bill_user_registration_form.php';
+      require_once 'inv_bill_user_login.php';
+
+      new invSettimngPage();
+      new userresgistration();
+      new userlogin();
+   }
+
+   /**
+    * Plugin Activatoin Hook
+    */
+
+   public function invintegrate_assets_activate()
+   {
+
+      $installed = get_option('invintegrate_installed');
+
+      if (!$installed) {
+         update_option('invintegrate_installed', time());
       }
 
-      update_option( 'invintegrate_assets_version', invINTEGRATE_ASSETS_VERSION );
-   } 
+      update_option('invintegrate_assets_version', invINTEGRATE_ASSETS_VERSION);
+   }
 }
 
 
- /**
-  * Create a class instance
-  */  
+/**
+ * Create a class instance
+ */
 
 
- new  invintegrate();   
-
+new invintegrate();
--- inv_bill_user_registration_form.php
+++ inv_bill_user_registration_form.php
@@ -1,90 +1,74 @@
-    <?php 
-    if ( ! defined('ABSPATH')) exit;  // if direct access 
+<?php
+if (!defined('ABSPATH'))
+    exit; // if direct access 
 
-    /**
-     *  User registration form
-    */
-    final class userresgistration{
+/**
+ *  User registration form
+ */
+final class userresgistration
+{
 
-     function __construct()
-     {
-
+    function __construct()
+    {
         add_action('wp_ajax_nopriv_inv_registeration_callback', [$this, 'inv_bill_registeration_form']);
-        
         add_action('wp_ajax_inv_registeration_callback', [$this, 'inv_bill_registeration_form']);
-
         add_shortcode('inv_user_regsitration_form', [$this, 'inv_user_regsitration']);
-
     }
 
-    function inv_user_regsitration(){
-
-        if ( is_user_logged_in() ) { 
+    function inv_user_regsitration()
+    {
+        if (is_user_logged_in()) {
             echo '<h6>You have already login</h6>';
         } else { ?>
             <div class="buyer_seller_reg_form">
-               <div class="menus_bs">
-                  <h4>Create your account</h4>
+                <div class="menus_bs">
+                    <h4>Create your account</h4>
 
-              </div>
-              <form method="POST" name="user_registeration">
-                <label>Username*</label>  
-                <input type="text" name="username" placeholder="Enter Your Username" id="inv_username" required />
-                <br/>
-                <label>Email address*</label>
-                <input type="text" name="useremail" id="inv_email" placeholder="Enter Your Email" required />
-                <br/>
-                <label>Password*</label>
-                <input type="password" name="password" id="inv_password" placeholder="Enter Your password" required /> 
+                </div>
+                <form method="POST" name="user_registeration">
+                    <label>Username*</label>
+                    <input type="text" name="username" placeholder="Enter Your Username" id="inv_username" required />
+                    <br />
+                    <label>Email address*</label>
+                    <input type="text" name="useremail" id="inv_email" placeholder="Enter Your Email" required />
+                    <br />
+                    <label>Password*</label>
+                    <input type="password" name="password" id="inv_password" placeholder="Enter Your password" required />
 
-                <br>
-                <input type="button" id="inv_registeration" value="SignUp" /> 
-
-            </form>
-            <span id="bs_error_message" style="color:darkred;"></span>
-
-            <?php 
-
-
-            ?>
-        </div>
-
-        <?php 
-    } 
-}
-
-/* ==== user Registration form ===== */
-
-function inv_bill_registeration_form() {
-
-    $inv_username  = $_REQUEST['inv_username'];
-    $inv_email  = $_REQUEST['inv_email'];
-    $inv_password  = $_REQUEST['inv_password'];
-    $exists = email_exists( $iv_email );
-    if ( $exists ) {
-        echo "That E-mail is already Exist";
-    } else {
-        $userdata = array(
-            'user_pass'   => $inv_password,  
-            'user_login'  => $inv_username, 
-            'user_nicename' => $inv_username,   
-            'user_email'   => $inv_email, 
-            'role'       => 'subscriber',   
-        );
-
-        $user_id=wp_insert_user( $userdata ) ;
-
-        if ( ! is_wp_error( $user_id ) ) {
-            echo "You have registered Successfully";            
+                    <br>
+                    <input type="button" id="inv_registeration" value="SignUp" />
+                </form>
+                <span id="bs_error_message" style="color:darkred;"></span>
+            </div>
+            <?php
         }
     }
 
-    wp_die();
+    /* ==== user Registration form ===== */
+    function inv_bill_registeration_form()
+    {
+        $inv_username = $_REQUEST['inv_username'];
+        $inv_email = $_REQUEST['inv_email'];
+        $inv_password = $_REQUEST['inv_password'];
+        $exists = email_exists($inv_email);
+        if ($exists) {
+            echo "That E-mail is already Exist";
+        } else {
+            $userdata = array(
+                'user_pass'     => $inv_password,
+                'user_login'    => $inv_username,
+                'user_nicename' => $inv_username,
+                'user_email'    => $inv_email,
+                'role'          => 'subscriber',
+            );
 
+            $user_id = wp_insert_user($userdata);
+
+            if (!is_wp_error($user_id)) {
+                echo "You have registered Successfully";
+            }
+        }
+
+        wp_die();
+    }
 }
-
-
-
-} /*end class*/
-
-
